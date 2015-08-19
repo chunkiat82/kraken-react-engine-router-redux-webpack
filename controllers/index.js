38f50fd45f0ace4a4f32dfdf4dc5a434e54f8589
Kathirvelu, Gayathri(gaykathirvelu) <gaykathirvelu@paypal.com>
2015-08-19 18:33:24 +0800

    yo generated files

---
+++ controllers/index.js
@@ -0,0 +1,33 @@
+/*-------------------------------------------------------------------------------------------------------------------*\
+ |  Copyright (C) 2015 PayPal                                                                                          |
+ |                                                                                                                     |
+ |  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     |
+ |  with the License.                                                                                                  |
+ |                                                                                                                     |
+ |  You may obtain a copy of the License at                                                                            |
+ |                                                                                                                     |
+ |       http://www.apache.org/licenses/LICENSE-2.0                                                                    |
+ |                                                                                                                     |
+ |  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   |
+ |  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  |
+ |  the specific language governing permissions and limitations under the License.                                     |
+ \*-------------------------------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+var IndexModel = require('../models/index');
+
+
+module.exports = function (router) {
+
+    var model = new IndexModel();
+
+    router.get('/', function (req, res) {
+        res.render(req.url, model);
+    });
+
+    router.get('/server', function(req, res) {
+        res.render('server', model);
+    });
+
+};
---
+++ test/index.js
@@ -0,0 +1,61 @@
+/*-------------------------------------------------------------------------------------------------------------------*\
+ |  Copyright (C) 2015 PayPal                                                                                          |
+ |                                                                                                                     |
+ |  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     |
+ |  with the License.                                                                                                  |
+ |                                                                                                                     |
+ |  You may obtain a copy of the License at                                                                            |
+ |                                                                                                                     |
+ |       http://www.apache.org/licenses/LICENSE-2.0                                                                    |
+ |                                                                                                                     |
+ |  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   |
+ |  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  |
+ |  the specific language governing permissions and limitations under the License.                                     |
+ \*-------------------------------------------------------------------------------------------------------------------*/
+
+/*global describe:false, it:false, beforeEach:false, afterEach:false*/
+
+'use strict';
+
+
+var kraken = require('kraken-js'),
+    express = require('express'),
+    request = require('supertest');
+
+
+describe('/', function () {
+
+    var app, mock;
+
+
+    beforeEach(function (done) {
+        app = express();
+        app.on('start', done);
+        app.use(kraken({
+            basedir: process.cwd()
+        }));
+
+        mock = app.listen(1337);
+
+    });
+
+
+    afterEach(function (done) {
+        mock.close(done);
+    });
+
+
+    it('should say "hello"', function (done) {
+        request(mock)
+            .get('/')
+            .expect(200)
+            .expect('Content-Type', /html/)
+            
+                .expect(/"name": "index"/)
+            
+            .end(function (err, res) {
+                done(err);
+            });
+    });
+
+});
