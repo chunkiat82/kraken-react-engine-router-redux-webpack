e0548e327c0b867ceb7ee665a72bd3241b3a415a
Raymond Ho <chunkiat82@gmail.com>
2015-10-26 09:34:59 +0800

    test of window element

--- src/public/views/helloWorld.jsx
+++ src/public/views/helloWorld.jsx
@@ -12,12 +12,24 @@ export default class HelloWorld extends React.Component {
     componentDidMount(){        
     }
 
-    render() {        
+    render() {
+        var fontSize  = 16;
+
+        // a test for client side objects
+        if (typeof window !== 'undefined') {
+            fontSize = window.innerHeight/30;
+        }
+
+        var styles= {
+            "fontSize":fontSize
+        }
+
         return (
             <Grid>                
                 <Row>
                     <Col xs={5} md={4}></Col>
                     <Col xs={2} md={4}>
+                        <div style={styles}>This is just to test the availability of Window Object</div>
                         <pre>
                             {JSON.stringify(convert({first_name:this.props.firstName, "last_name":this.props.lastName}))}
                         </pre>
