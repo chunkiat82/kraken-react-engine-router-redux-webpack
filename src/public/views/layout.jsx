14e2c894b9e0892e145449406cc3e0ba9d3169ad
Raymond Ho <chunkiat82@gmail.com>
2015-11-15 16:53:00 +0800

    fixing DEVELOPMENT check for dev tools

--- src/public/views/layout.jsx
+++ src/public/views/layout.jsx
@@ -1,20 +1,4 @@
-/*-------------------------------------------------------------------------------------------------------------------*\
- |  Copyright (C) 2015 PayPal                                                                                          |
- |                                                                                                                     |
- |  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     |
- |  with the License.                                                                                                  |
- |                                                                                                                     |
- |  You may obtain a copy of the License at                                                                            |
- |                                                                                                                     |
- |       http://www.apache.org/licenses/LICENSE-2.0                                                                    |
- |                                                                                                                     |
- |  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   |
- |  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  |
- |  the specific language governing permissions and limitations under the License.                                     |
- \*-------------------------------------------------------------------------------------------------------------------*/
-
-'use strict';
-
+/* global __DEVELOPMENT__:true */
 import React from 'react';
 import Header from './header.jsx';
 import PageSlider from 'react-page-slider';
@@ -25,6 +9,18 @@ import * as CounterActions from '../../actions/counter';
 import DevTools from '../../containers/DevTools';
 
 class Layout extends React.Component {
+
+	constructor(){
+		super();
+		this.renderDevTools = this.renderDevTools.bind(this);
+	}
+
+	renderDevTools(){
+		if (__DEVELOPMENT__) {
+			return (<DevTools />);
+		}
+		return '';
+	}
 
 	render() {
 		const { step,fieldName, getValues, increment, incrementIfOdd, incrementAsync, decrement, counter } = this.props;
@@ -57,9 +53,7 @@ class Layout extends React.Component {
 							{'Click on \'close\' to dismiss this overlay'}
 						</div>
 					</PageSlider>
-					if (__DEVELOPMENT__) {
-						<DevTools />
-					}
+					{this.renderDevTools()}
 				  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 				  	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 				  	<script src='/bundle.js'></script>
